785c10dc7fc77d98ea70de0b34076d7ba0e06cbe
sagisaka3301 <s.kazani777@gmail.com>
2023-06-11 17:13:04 +0900

    styling

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,10 +2,31 @@
     <x-slot name="head">
         <link rel="stylesheet" href="{{ asset('/css/top.css') }}">
         <link rel="stylesheet" href="{{ asset('/css/create.css') }}">
+        <link href="https://fonts.googleapis.com/css?family=Corben:700" rel="stylesheet">
     </x-slot>
-    <div class="first-view" style="background-image: url({{ asset('images/paper.jpg') }});">
-        <h1 class="abs-center">投稿作成！！！</h1>
-    </div>
+    <div class="first-view" style="background-image: url({{ asset('images/space.jpg') }});">
+            <div class="fv-inner">
+                <h1 class="abs-center back-title">Team Development</h1>
+                <h1 class="title abs-center">
+                  <span>T</span>
+                  <span>e</span>
+                  <span>a</span>
+                  <span>m</span>
+                  <span>　</span>
+                  <span>D</span>
+                  <span>e</span>
+                  <span>v</span>
+                  <span>e</span>
+                  <span>l</span>
+                  <span>o</span>
+                  <span>p</span>
+                  <span>m</span>
+                  <span>e</span>
+                  <span>n</span>
+                  <span>t</span>
+                </h1>
+            </div>
+        </div>
     <div class="blog-container create-container">
         <div class="create-inner">
             <form action="/posts" method="POST">
@@ -46,4 +67,5 @@
             </div>
         </div>
     </div>
+    <script src="{{ asset('js/top.js') }}"></script>
 </x-main-layout>
