409bfb24d927c9a376e89eae84926c7e3f48b695
Ruii1112 <ruiiiii1112@gmail.com>
2023-06-11 17:12:21 +0900

    fix create.blade

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -19,7 +19,9 @@ class PostSeeder extends Seeder
         DB::table('posts')->insert([
             'title' => 'チーム開発会って？',
             'body' => 'チームで協力して一つの成果物を作るイベントです！メンバー全員で助け合いましょう！',
+            'translation' => 'This is an event where teams work together to create a single deliverable! All members are encouraged to help each other!',
             'category_id' => 1,
+            'user_id' => 1,
             'created_at' => new DateTime(),
             'updated_at' => new DateTime(),
         ]);
@@ -27,7 +29,9 @@ class PostSeeder extends Seeder
         DB::table('posts')->insert([
             'title' => '役割分担',
             'body' => 'これはborderという、cssでつけることができる枠線です！'.PHP_EOL.'太さの指定や形など色々指定できるので、気になった方はコードを覗いてみたり、調べてみたりしましょう！'.PHP_EOL.'また、このプロジェクト内ではインラインCSSという、HTML内に書く簡易的なCSSを使用しています！こちらも気になった方は見てみてください！',
+            'translation' => 'This is a border, which can be added with css!'.PHP_EOL.'You can specify the thickness, shape, and many other things, so if you are interested, take a peek at the code and check it out!'.PHP_EOL.'We also use inline CSS within this project, which is simple CSS that is written within HTML! Please take a look at this as well if you are interested!',
             'category_id' => 2,
+            'user_id' => 2,
             'created_at' => new DateTime(),
             'updated_at' => new DateTime(),
         ]);
@@ -35,7 +39,9 @@ class PostSeeder extends Seeder
         DB::table('posts')->insert([
             'title' => 'この枠線みたいなやつって何？',
             'body' => '開発を進める際は、役割分担をすると効率的に開発をすることができます！'.PHP_EOL.'具',
+            'translation' => 'When proceeding with development, dividing the roles among them allows for more efficient development!'.PHP_EOL.'ingredients',
             'category_id' => 2,
+            'user_id' => 1,
             'created_at' => new DateTime(),
             'updated_at' => new DateTime(),
         ]);
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,33 +2,53 @@
     <x-slot name="head">
         <link rel="stylesheet" href="{{ asset('/css/top.css') }}">
         <link rel="stylesheet" href="{{ asset('/css/create.css') }}">
+        <link href="https://fonts.googleapis.com/css?family=Corben:700" rel="stylesheet">
     </x-slot>
-    <div class="first-view" style="background-image: url({{ asset('images/paper.jpg') }});">
-        <h1 class="abs-center">投稿作成！！！</h1>
+    <div class="first-view" style="background-image: url({{ asset('images/space.jpg') }});">
+        <div class="fv-inner">
+            <h1 class="abs-center back-title">Team Development</h1>
+            <h1 class="title abs-center">
+              <span>T</span>
+              <span>e</span>
+              <span>a</span>
+              <span>m</span>
+              <span>　</span>
+              <span>D</span>
+              <span>e</span>
+              <span>v</span>
+              <span>e</span>
+              <span>l</span>
+              <span>o</span>
+              <span>p</span>
+              <span>m</span>
+              <span>e</span>
+              <span>n</span>
+              <span>t</span>
+            </h1>
+        </div>
     </div>
     <div class="blog-container create-container">
         <div class="create-inner">
+            
             <form action="/posts" method="POST">
                 @csrf
-                <!--vakidate message-->
-                @if ($errors->any())
-                    <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                    </ul>
-                @endif
                 
+                @error('post.title')
+                    <p>{{ $message }}</p>
+                @enderror
+                
+                @error('post.body')
+                    <p>{{ $message }}</p>
+                @enderror
+            
                 <div class="input-row name-input flex">
                     <h2>タイトル</h2>
                     <input type="text" name="post[title]" placeholder="タイトル" value="{{ old('post.title') }}"/>
                 </div>
-                <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
                 <div class="input-row body-input flex">
                     <h2>本文</h2>
                     <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。">{{ old('post.body') }}</textarea>
                 </div>
-                <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
                 <div class="input-row category-input flex">
                     <h2>カテゴリー</h2>
                     <select name="post[category_id]">
@@ -46,4 +66,5 @@
             </div>
         </div>
     </div>
+    <script src="{{ asset('js/top.js') }}"></script>
 </x-main-layout>
